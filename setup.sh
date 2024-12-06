3f47a3583e1d3365df1094c808154055445f2b23
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-12-06 11:07:48 +0000

    Removed shell script
    Better to load modules separately and then directly run nextflow - makes it easier to change nextflow flags

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
+#!/bin/bash
 ml load Python/3.11.5-GCCcore-13.2.0
 ml load Nextflow/23.10.0
 ml load Java/11.0.20
 source ~/venvs/cellphe/bin/activate
-
-nextflow run process_dataset.nf --dataset $1 -resume
-
-deactivate
-
