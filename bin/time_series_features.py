c6e5388b76993b0b304fa622836d8b3fcb01c4cd
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-12-06 11:09:26 +0000

    Only aggregate cells that are in 50 frames

--- bin/time_series_features.py
+++ bin/time_series_features.py
@@ -14,15 +14,15 @@ args = parser.parse_args()
 new_features = pd.read_csv(args.frame_file)
 cell_counts = new_features["CellID"].value_counts()
 
-#if any(cell_counts > 5):
+if any(cell_counts > 5):
     # Filter new_features to include only cells with more than 3 frames
-    #valid_cells = cell_counts[cell_counts > 3].index
-    #filtered_features = new_features[
-    #    new_features["CellID"].isin(valid_cells)
-    #]
+    valid_cells = cell_counts[cell_counts > 50].index
+    filtered_features = new_features[
+        new_features["CellID"].isin(valid_cells)
+    ]
 
     # Extract time series features
-tsvariables = time_series_features(new_features)
+tsvariables = time_series_features(filtered_features)
 
 # Save the new features to a CSV file
 tsvariables.to_csv(args.time_series_file, index=False)
