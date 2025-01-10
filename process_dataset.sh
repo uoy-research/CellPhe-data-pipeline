6494186f9884d1234edc9f6e4cca099d937e52bf
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-01-10 12:11:59 +0000

    Added wrapper script to load modules first

---
+++ process_dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Wrapper for the NextFlow job submission that sets the environment before running the job
+# Args:
+#   - 1: Dataset name
+ml load Python/3.11.5-GCCcore-13.2.0
+ml load Nextflow/23.10.0
+ml load Java/11.0.20
+source ~/venvs/cellphe/bin/activate
+nextflow run process_dataset.nf --dataset $1
--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ml load Python/3.11.5-GCCcore-13.2.0
-ml load Nextflow/23.10.0
-ml load Java/11.0.20
-source ~/venvs/cellphe/bin/activate
