dd172d4060b6599c8856a57199e403b25c04d93b
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-01-13 14:06:26 +0000

    Split multi-channel .ome.tiff into frame-specific tiffs

--- process_dataset.sh
+++ process_dataset.sh
@@ -5,5 +5,7 @@
 ml load Python/3.11.5-GCCcore-13.2.0
 ml load Nextflow/23.10.0
 ml load Java/11.0.20
+ml load Perl-bundle-CPAN/5.38.0-GCCcore-13.2.0
+ml load LibTIFF/4.6.0-GCCcore-13.2.0
 source ~/venvs/cellphe/bin/activate
 nextflow run process_dataset.nf --dataset $1
