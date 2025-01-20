88c3abeb2f736a071a8a2ca8b00f2924bad658b9
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-01-20 15:19:56 +0000

    Use a venv from the project folder rather than a personal one

--- process_dataset.sh
+++ process_dataset.sh
@@ -7,5 +7,5 @@ ml load Nextflow/23.10.0
 ml load Java/11.0.20
 ml load Perl-bundle-CPAN/5.38.0-GCCcore-13.2.0
 ml load LibTIFF/4.6.0-GCCcore-13.2.0
-source ~/venvs/cellphe/bin/activate
+source /mnt/scratch/projects/biol-imaging-2024/venv/bin/activate
 nextflow run process_dataset.nf --dataset $1
