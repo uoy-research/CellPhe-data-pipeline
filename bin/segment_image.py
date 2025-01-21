325b19efd03d81c1742e1e505e6729b2552e7bbb
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-01-21 12:49:06 +0000

    Changed to cyto3 modle

--- bin/segment_image.py
+++ bin/segment_image.py
@@ -9,11 +9,11 @@ from skimage import io
 parser = argparse.ArgumentParser(
                     description='Creates the segmentation mask for a single image using CellPose'
 )
-parser.add_argument('input', help="Path to the raw TIF")
-parser.add_argument('output', help="Path to the output TIF mask")
+parser.add_argument('input', help="Path to the raw image")
+parser.add_argument('output', help="Path to the output mask")
 args = parser.parse_args()
 
-model = models.Cellpose(gpu=False, model_type="cyto")
+model = models.Cellpose(gpu=False, model_type="cyto3")
 image = read_tiff(args.input)
 masks, _, _, _ = model.eval(image)
 io.imsave(args.output, masks.astype("uint16"))  # Assuming masks are uint16
