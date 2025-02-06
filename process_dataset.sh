de1eae68a19d5737b0b08819e69d9331915037fe
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-02-06 22:06:08 +0000

    Add maven to PATH

--- process_dataset.sh
+++ process_dataset.sh
@@ -7,8 +7,9 @@ EXPERIMENT=${1}
 CONFIG=${2}
 RESUME=${3:-default}
 
-EXPERIMENT_DIR="/mnt/scratch/projects/biol-imaging-2024/Experiments/$EXPERIMENT"
-NEXTFLOW_FILE="/mnt/scratch/projects/biol-imaging-2024/CellPhe-data-pipeline/process_dataset.nf"
+PROJECT_DIR="/mnt/scratch/projects/biol-imaging-2024/"
+EXPERIMENT_DIR="$PROJECT_DIR/Experiments/$EXPERIMENT"
+NEXTFLOW_FILE="$PROJECT_DIR/CellPhe-data-pipeline/process_dataset.nf"
 
 ml load Python/3.11.5-GCCcore-13.2.0
 ml load Nextflow/23.10.0
@@ -19,8 +20,10 @@ ml load ImageMagick/7.1.1-34-GCCcore-13.2.0
 ml load Quarto/1.6.39-x86_64-linux
 ml load R/4.4.1-gfbf-2023b
 ml load R-bundle-CRAN/2024.06-foss-2023b
-source /mnt/scratch/projects/biol-imaging-2024/venv/bin/activate
-export CELLPOSE_LOCAL_MODELS_PATH=/mnt/scratch/projects/biol-imaging-2024/cellpose
+source $PROJECT_DIR/venv/bin/activate
+export CELLPOSE_LOCAL_MODELS_PATH=$PROJECT_DIR/cellpose
+export PATH=$PATH:$PROJECT_DIR/bin/apache-maven-3.9.9/bin
+
 
 cd $EXPERIMENT_DIR
 CMD="srun --ntasks=1 --cpus-per-task 4 --mem=8G --time=120 nextflow run $NEXTFLOW_FILE -work-dir .work -params-file $CONFIG -ansi-log true"
