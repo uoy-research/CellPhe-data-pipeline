20e2f98c6e47f49b6217edd093fce71d098b7701
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-02-06 10:51:25 +0000

    Store configs in separate 'configs' dir

--- run.sh
+++ run.sh
@@ -21,18 +21,21 @@ SITE=$(../tools/jq -r .folder_names.site $CONFIG)
 IMAGE=$(../tools/jq -r .folder_names.image_type $CONFIG)
 # TODO should add hash or something to make this unique
 CONFIG_FN="config_${SITE}_${IMAGE}.json"
+EXPERIMENT_PATH="/mnt/scratch/projects/biol-imaging-2024/Experiments/$EXPERIMENT"
+CONFIG_PATH="$EXPERIMENT_PATH/configs/$CONFIG_FN"
+
 ml load tools/rclone
 
 # Step 1: Transfer data and config to Viking
 echo "Transferring data to Viking..."
-rclone --config .rclone.config copy -v --include "*$PATTERN*.companion.ome*" --include "*$PATTERN*.tif" --include "*$PATTERN*.tiff" --include "*$PATTERN*.TIF" --include "*$PATTERN*.TIFF" --include "*$PATTERN*.jpg" --include "*$PATTERN*.jpeg" --include "*$PATTERN*.JPG" --include "*$PATTERN*.JPEG" --drive-root-folder-id $GDRIVE_ID GDrive: Viking:/mnt/scratch/projects/biol-imaging-2024/Experiments/$EXPERIMENT/raw/${SITE}_${IMAGE}
-rclone --config .rclone.config copyto -v $CONFIG Viking:/mnt/scratch/projects/biol-imaging-2024/Experiments/$EXPERIMENT/$CONFIG_FN
+rclone --config .rclone.config copy -v --include "*$PATTERN*.companion.ome*" --include "*$PATTERN*.tif" --include "*$PATTERN*.tiff" --include "*$PATTERN*.TIF" --include "*$PATTERN*.TIFF" --include "*$PATTERN*.jpg" --include "*$PATTERN*.jpeg" --include "*$PATTERN*.JPG" --include "*$PATTERN*.JPEG" --drive-root-folder-id $GDRIVE_ID GDrive: Viking:$EXPERIMENT_PATH/raw/${SITE}_${IMAGE}
+rclone --config .rclone.config copyto -v $CONFIG Viking:$CONFIG_PATH
 
 # Step 2: Submit the job to process the data (waits until complete)
 echo "Executing pipeline..."
-NEXTFLOW_CMD="cd /mnt/scratch/projects/biol-imaging-2024/CellPhe-data-pipeline && ./process_dataset.sh $EXPERIMENT $CONFIG_FN $RESUME"
+NEXTFLOW_CMD="cd /mnt/scratch/projects/biol-imaging-2024/CellPhe-data-pipeline && ./process_dataset.sh $EXPERIMENT $CONFIG_PATH $RESUME"
 ssh viking "${NEXTFLOW_CMD}"
 
 # Step 3: Transfer outputs to network share
 echo "Transferring outputs to bioldata..."
-rclone --config .rclone.config copy --no-update-modtime --exclude ".work/**" --exclude ".nextflow**" -v Viking:/mnt/scratch/projects/biol-imaging-2024/Experiments/$EXPERIMENT /shared/storage/bioldata/bl-cellphe/Experiments/$EXPERIMENT
+rclone --config .rclone.config copy --no-update-modtime --exclude ".work/**" --exclude ".nextflow**" -v Viking:$EXPERIMENT_PATH /shared/storage/bioldata/bl-cellphe/Experiments/$EXPERIMENT
