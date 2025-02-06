f129697cdc472c1f23737ab65047d1d369944c92
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-02-06 00:36:02 +0000

    Updated launcher script & config to handle sites and image types
    Also allows for the specification of which parts of the pipeline get run

--- run.sh
+++ run.sh
@@ -3,23 +3,36 @@
 # Needs rclone setup with remotes CellPheGDrive, CellPheViking (might be able 
 # to be setup automatically later)
 #
+# NB: This script is currently designed to be run for a single timelapse,
+# i.e. one well and one imaging modality.
+#
 # Args:
 #   - source: Google Drive folder ID
-#   - dataset: Dataset name for the output
+#   - experiment: Experiment name for the output, should be in the format
+#   YYYY-mm-dd_experiment-name_microscope_cellline
 #   - pattern: Any file matching pattern, i.e. "Brightfield-*.tiff"
+#   - config: Config file providing pipeline parameters
 GDRIVE_ID=${1}
-DATASET=${2}
+EXPERIMENT=${2}
 PATTERN=${3}
 CONFIG=${4}
+RESUME=${5:-default}
+SITE=$(../tools/jq -r .folder_names.site $CONFIG)
+IMAGE=$(../tools/jq -r .folder_names.image_type $CONFIG)
+# TODO should add hash or something to make this unique
+CONFIG_FN="config_${SITE}_${IMAGE}.json"
 ml load tools/rclone
 
 # Step 1: Transfer data and config to Viking
-rclone --config .rclone.config copy -v --include "*$PATTERN*.companion.ome*" --include "*$PATTERN*.tif" --include "*$PATTERN*.tiff" --include "*$PATTERN*.TIF" --include "*$PATTERN*.TIFF" --include "*$PATTERN*.jpg" --include "*$PATTERN*.jpeg" --include "*$PATTERN*.JPG" --include "*$PATTERN*.JPEG" --drive-root-folder-id $GDRIVE_ID GDrive: Viking:/mnt/scratch/projects/biol-imaging-2024/Datasets/$DATASET/raw
-rclone --config .rclone.config copyto -v $CONFIG Viking:/mnt/scratch/projects/biol-imaging-2024/Datasets/$DATASET/config.json
+echo "Transferring data to Viking..."
+rclone --config .rclone.config copy -v --include "*$PATTERN*.companion.ome*" --include "*$PATTERN*.tif" --include "*$PATTERN*.tiff" --include "*$PATTERN*.TIF" --include "*$PATTERN*.TIFF" --include "*$PATTERN*.jpg" --include "*$PATTERN*.jpeg" --include "*$PATTERN*.JPG" --include "*$PATTERN*.JPEG" --drive-root-folder-id $GDRIVE_ID GDrive: Viking:/mnt/scratch/projects/biol-imaging-2024/Experiments/$EXPERIMENT/raw/${SITE}_${IMAGE}
+rclone --config .rclone.config copyto -v $CONFIG Viking:/mnt/scratch/projects/biol-imaging-2024/Experiments/$EXPERIMENT/$CONFIG_FN
 
 # Step 2: Submit the job to process the data (waits until complete)
-NEXTFLOW_CMD="cd /mnt/scratch/projects/biol-imaging-2024/CellPhe-data-pipeline && ./process_dataset.sh $DATASET"
+echo "Executing pipeline..."
+NEXTFLOW_CMD="cd /mnt/scratch/projects/biol-imaging-2024/CellPhe-data-pipeline && ./process_dataset.sh $EXPERIMENT $CONFIG_FN $RESUME"
 ssh viking "${NEXTFLOW_CMD}"
 
 # Step 3: Transfer outputs to network share
-rclone --config .rclone.config copy --exclude ".work/**" -v Viking:/mnt/scratch/projects/biol-imaging-2024/Datasets/$DATASET /shared/storage/bioldata/bl-cellphe/Datasets/$DATASET
+echo "Transferring outputs to bioldata..."
+rclone --config .rclone.config copy --exclude ".work/**" --exclude ".nextflow**" -v Viking:/mnt/scratch/projects/biol-imaging-2024/Experiments/$EXPERIMENT /shared/storage/bioldata/bl-cellphe/Experiments/$EXPERIMENT
