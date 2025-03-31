62ddc3b9f1402ebf2a880e8a162146af69f70ca3
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-03-31 14:28:26 +0100

    hotfix: use timelapse as launch dir

--- process_dataset.sh
+++ process_dataset.sh
@@ -30,9 +30,7 @@ export PATH=$PATH:$PROJECT_DIR/bin/apache-maven-3.9.9/bin
 # Run pipeline from a directory specific to this timelapse
 SITE=$(jq -r .folder_names.site $CONFIG)
 IMAGE=$(jq -r .folder_names.image_type $CONFIG)
-SEG=$(jq -r .folder_names.segmentation $CONFIG)
-TRACK=$(jq -r .folder_names.tracking $CONFIG)
-LAUNCH_DIR="$EXPERIMENT_DIR/.launch/${SITE}_${IMAGE}_${SEG}_${TRACK}"
+LAUNCH_DIR="$EXPERIMENT_DIR/.launch/${SITE}_${IMAGE}"
 mkdir -p $LAUNCH_DIR
 cd $LAUNCH_DIR
 CMD="srun --ntasks=1 --cpus-per-task 4 --mem=8G --time=120 nextflow run $NEXTFLOW_FILE -work-dir .work -params-file $CONFIG -ansi-log true"
