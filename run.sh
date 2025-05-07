def9f0407049052a37fe09134f288d7ddf700fb5
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-05-07 23:33:17 +0100

    Allow for running the pipeline from data on bioldata
    Previously the launching script required raw data to be stored on Google
    Drive. It is now more flexible and can handle a path to a folder
    containing the data on bioldata instead, along with the `-b` flag.

--- run.sh
+++ run.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 # Main script to run the entire workflow. Must be run from research0.
-# Needs rclone setup with remotes CellPheGDrive, CellPheViking (might be able 
-# to be setup automatically later)
+# Needs rclone setup with remotes CellPheViking and CellPheGDrive (if accessing datasets on Google Drive).
 #
 # NB: This script is currently designed to be run for a single timelapse,
 # i.e. one well and one imaging modality.
 #
 # Args:
-#   - 1: source: Google Drive folder ID
-#   YYYY-mm-dd_experiment-name_microscope_cellline
-#   - 2: pattern: Any file matching pattern, i.e. "Brightfield-*.tiff"
-#   - 3: config: Config file providing pipeline parameters. Must be saved in the configs sub-directory of an Experiment
-GDRIVE_ID=${1}
-PATTERN=${2}
-CONFIG=${3}
+#   - 1: -b (optional). Whether to interpret the source as a bioldata folder. If not present assumes it is a GoogleDrive ID
+#   - 2: source: Google Drive folder ID OR path to bioldata folder
+#   - 3: pattern: Any file matching pattern, i.e. "Brightfield-*.tiff"
+#   - 4: config: Config file providing pipeline parameters. Must be saved in the configs sub-directory of an Experiment
+
+print_usage() {
+    printf "Usage: run.sh GDRIVEID B2 /path/to/config.json OR run.sh -b /path/to/folder/on/bioldata B2 /path/to/config.json"
+}
+
+bioldata=false
+
+while getopts 'b' flag; do
+  case "${flag}" in
+    b) bioldata=true ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+SOURCE=${@:$OPTIND:1}
+PATTERN=${@:$OPTIND+1:1}
+CONFIG=${@:$OPTIND+2:1}
+
+echo "Source = $SOURCE"
+echo "Pattern = $PATTERN"
+echo "Config = $CONFIG"
+echo "bioldata = $bioldata"
 
 # Validate the provided config file
 EXPERIMENT="$(basename $(dirname $(dirname ${CONFIG})))"
@@ -37,8 +55,23 @@ RAW_DATA_DIR="$EXPERIMENT_PATH_VIKING/raw/${SITE}_${IMAGE}"
 ml load tools/rclone
 
 # Step 1: Transfer data and config to Viking
+OUTBOUND_CMD="rclone --config .rclone.config copy --stats-log-level NOTICE --include \"*$PATTERN*.companion.ome*\" --include \"*$PATTERN*.tif\" --include \"*$PATTERN*.tiff\" --include \"*$PATTERN*.TIF\" --include \"*$PATTERN*.TIFF\" --include \"*$PATTERN*.jpg\" --include \"*$PATTERN*.jpeg\" --include \"*$PATTERN*.JPG\" --include \"*$PATTERN*.JPEG\""
+
+# Add source folder
+if [[ $bioldata = true ]]
+then
+    OUTBOUND_CMD="$OUTBOUND_CMD \"$SOURCE\""
+else
+    OUTBOUND_CMD="$OUTBOUND_CMD --drive-root-folder-id $SOURCE GDrive:"
+fi
+
+# Destination is always the same
+OUTBOUND_CMD="$OUTBOUND_CMD Viking:$RAW_DATA_DIR"
+
+# Copy data to viking
 echo "Transferring data to Viking..."
-rclone --config .rclone.config copy --stats-log-level NOTICE --include "*$PATTERN*.companion.ome*" --include "*$PATTERN*.tif" --include "*$PATTERN*.tiff" --include "*$PATTERN*.TIF" --include "*$PATTERN*.TIFF" --include "*$PATTERN*.jpg" --include "*$PATTERN*.jpeg" --include "*$PATTERN*.JPG" --include "*$PATTERN*.JPEG" --drive-root-folder-id $GDRIVE_ID GDrive: Viking:$RAW_DATA_DIR
+eval $OUTBOUND_CMD
+# Also copy config file
 rclone --config .rclone.config copyto -v $CONFIG Viking:$CONFIG_PATH_VIKING
 
 # Step 2: Submit the job to process the data (waits until complete)
