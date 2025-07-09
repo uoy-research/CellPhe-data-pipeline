d644c94c662b67c17a99cf66c75b7c2c6666128b
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-07-09 13:30:27 +0100

    feat: unique `TRACK_ID` per frame and overlay plot in tracking QC report (#62)
    * Add plot showing cell movement
    
    * First effort in obtaining unique TRACK_IDs per frame
    
    A graph is formed of all the nodes, which is traversed recurively to
    build up a list of tracks, where each track only has 1 cell id per
    frame associated with it.
    This is the default PhaseFocus behaviour, but isn't the case in
    TrackMate, where multiple cells in the same frame were assigned the same
    track id, as Trackmate treats track id as an identifier of lineage.
    
    NB: this is a WIP, and doesn't fully work. It is currently outputting
    multiple TRACK_IDs for the same cell in the same frame.
    
    * Handle merge events
    
    * Added docs

--- bin/parse_xml.py
+++ bin/parse_xml.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import argparse
+from dataclasses import dataclass, field
 import json
 import sys
+from typing import List
 import xml.etree.ElementTree as ET
 import scyjava as sj
 import numpy as np
@@ -20,11 +22,22 @@ args = parser.parse_args()
 tree = ET.parse(args.xml_path)
 spot_records = []
 rois = {}
-# Get all Spots firstly
+
+# Get all Spots
+nodes = {}
+@dataclass
+class Node:
+    id: int
+    frame: int
+    children: List[id] = field(default_factory=list)
+    parents: List[id] = field(default_factory=list)
+
 for frame in tree.findall("./Model/AllSpots/SpotsInFrame"):
     # Get all spots, reading in their attributes and ROIs
     for spot in frame.findall("Spot"):
         spot_records.append(spot.attrib)
+        node_id = int(spot.attrib['ID'])
+        nodes[node_id] = Node(id=node_id, frame=int(spot.attrib['FRAME']))
         # Read ROIs
         coords = np.array([spot.text.split(" ")]).astype(float)
         coords = coords.reshape(int(coords.size / 2), 2)
@@ -33,20 +46,71 @@ for frame in tree.findall("./Model/AllSpots/SpotsInFrame"):
         rois[spot.attrib["name"]] = coords
 spot_df = pd.DataFrame.from_records(spot_records)
 spot_df = spot_df.rename(columns={"name": "LABEL"})
+spot_df['ID'] = spot_df['ID'].astype('int')
+spot_df['FRAME'] = spot_df['FRAME'].astype('int')
 
-# Then get all Tracks so can add TRACK_ID
-# Tracks are stored as edges between source and target cells in consecutive
-# frames. To get the unique cell ids in each track, store both source and
-# target and remove duplicates after. More memory intensive but simple
-track_records = []
+# Add edges to children and parents
 for track in tree.findall("./Model/AllTracks/Track"):
     for edge in track.findall("Edge"):
-        track_records.append({"TRACK_ID": track.attrib["TRACK_ID"], "ID": edge.attrib["SPOT_TARGET_ID"]})
-        track_records.append({"TRACK_ID": track.attrib["TRACK_ID"], "ID": edge.attrib["SPOT_SOURCE_ID"]})
-track_df = pd.DataFrame.from_records(track_records).drop_duplicates()
+        source_id = int(edge.attrib["SPOT_SOURCE_ID"])
+        target_id = int(edge.attrib["SPOT_TARGET_ID"])
+        nodes[source_id].children.append(target_id)
+        nodes[target_id].parents.append(source_id)
 
-# Combine Spots and Tracks
+# Remove nodes with no parents or children - not part of any track
+for id in list(nodes.keys()):
+    if len(nodes[id].parents) == 0 and len(nodes[id].children) == 0:
+        nodes.pop(id)
+
+# Obtain root nodes in frame order
+root_nodes = [n for id, n in nodes.items() if len(n.parents) == 0]
+root_nodes.sort(key = lambda x: x.frame)
+
+# Iterate through graph, creating unique track ids
+track_records = []
+track_id = 0
+traversed_nodes = set()
+def traverse_track(node, accum=False):
+    """
+    Recursive function to traverse a graph representing tracks between
+    cells in a timelapse.
+    A global counter keeps track of the current track id, and is incremented
+    whenever a split event is reached (defined as a parent having more than
+    1 child).
+
+    Args:
+        - node (Node): The current node being visited.
+        - accum (bool): Whether to accumulate the track_id counter in case a
+          split event is reached.
+
+    Returns:
+        None. Populates a global variable `track_records` with visited nodes
+        and their newly assigned track_id instead.
+    """
+    global track_id
+
+    # Prevent multiple tracks on a merge
+    if node.id in traversed_nodes:
+        return
+
+    # Assign different track_ids after a split
+    if accum:
+        track_id += 1
+
+    track_records.append({'ID': node.id, 'TRACK_ID': track_id})
+    traversed_nodes.add(node.id)
+    for j, child in enumerate(node.children):
+        traverse_track(nodes[child], j > 0)
+
+# Traverse graph starting from from all root nodes, each of which will
+# accumulate the track counter.
+for i, root in enumerate(root_nodes):
+    traverse_track(root, i > 0)
+
+# Combine Spots and Tracks into single DataFrame
+track_df = pd.DataFrame.from_records(track_records)
 comb_df = pd.merge(spot_df, track_df, on="ID")
+
 # Reorder columns to be the same as exported from the GUI
 col_order = [
     "LABEL",
@@ -83,8 +147,8 @@ col_order = [
 comb_df = comb_df[col_order]
 
 # Want CellID and FrameID to be 1-indexed
-comb_df["TRACK_ID"] = comb_df["TRACK_ID"].astype(int) + 1
-comb_df["FRAME"] = comb_df["FRAME"].astype(int) + 1
+comb_df["TRACK_ID"] = comb_df["TRACK_ID"] + 1
+comb_df["FRAME"] = comb_df["FRAME"] + 1
 # Create a ROI filename column - 0 padded
 n_digits_track_id = len(str(np.max(comb_df["TRACK_ID"])))
 n_digits_frame_id = len(str(np.max(comb_df["FRAME"])))
