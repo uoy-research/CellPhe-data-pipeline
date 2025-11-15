2d2f26413d6d858d94b4add626c4b62201b2cf1c
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-11-15 21:20:30 +0000

    Removed cellphe as a dependency
    Export the relevant code snippets from the CellPhe python package so
    that the only dependencies are `scyjava` and `pyimagej`

--- bin/track_images.py
+++ bin/track_images.py
@@ -3,8 +3,124 @@ import argparse
 import json
 import sys
 import scyjava as sj
-from cellphe.imagej import read_image_stack, setup_imagej
-from cellphe.trackmate import configure_trackmate, get_trackmate_xml, load_detector, load_tracker
+import imagej
+
+def setup_imagej(max_heap: int | None = None) -> None:
+    """
+    Sets up a JVM with ImageJ and the TrackMate plugin loaded.
+
+    :param max_heap: Size in GB of the maximum heap size allocated to the JVM.
+    Use if you are encountering memory problems with large datasets. Be careful
+    when using this parameter, the rule of thumb is not to assign more than 80%
+    of your computer's available memory.
+    :return: None, although could be updated to return reference to a Java class
+    net.imagej.ImageJ.
+    """
+    if max_heap is not None and isinstance(max_heap, int) and max_heap > 0:
+        sj.config.add_option(f"-Xmx{max_heap}g")
+    imagej.init(["net.imagej:imagej", "sc.fiji:TrackMate:7.13.2"], add_legacy=False)
+
+
+def read_image_stack(image_dir: str):
+    """
+    Reads a directory containing TIFs into ImageJ as a stack.
+
+    :param dir: Directory where the TIFs are located.
+    :return: An ImagePlus instance containing an ImageStack.
+    """
+    # pylint doesn't like the camel case naming. I think it helps readability as
+    # it denotes a Java class
+    # pylint: disable=invalid-name
+    FolderOpener = sj.jimport("ij.plugin.FolderOpener")
+    # Load all images as framestack
+    imp = FolderOpener.open(image_dir, "")
+
+    # When reading in the imagestack, the the number of frames is often
+    # (always?) interpreted as the number of channels. This corrects that in the
+    # same way as the info box that pops up when using TrackMate in the GUI
+    # Dims are ordered X Y Z C T
+    dims = imp.getDimensions()
+    if dims[4] == 1:
+        # If time dimension is actually in Z, swap Z & T
+        if dims[2] > 1:
+            imp.setDimensions(dims[4], dims[3], dims[2])
+        # If time dimension is actually in channels (usual case), swap C & T
+        elif dims[3] > 1:
+            imp.setDimensions(dims[2], dims[4], dims[3])
+        # If none of Z, C, T contain more than 1 (i.e. time), then we have an
+        # error
+        else:
+            raise ValueError(
+                f"""Time-dimension could not be identified as none of the Z, C, or T
+                channels contain more than 1 value: {dims[2:]}"""
+            )
+    return imp
+
+
+def load_detector(settings) -> None:
+    """
+    Loads a TrackMate detector.
+    Currently hardcoded to be the LabelImageDetector, as this works with the
+    labelled masks output from Cellpose.
+
+    :param settings: An instance of the Java class
+        fiji.plugin.Trackmate.Settings
+    :return: None, updates settings as a side-effect.
+    """
+    settings.detectorFactory = sj.jimport("fiji.plugin.trackmate.detection.LabelImageDetectorFactory")()
+    settings.detectorSettings = settings.detectorFactory.getDefaultSettings()
+
+
+def load_tracker(settings, tracker: str, tracker_settings: dict) -> None:
+    """
+    Loads a TrackMate tracker.
+
+    :param settings: An instance of the Java class
+        fiji.plugin.Trackmate.Settings
+    :param tracker: String specifying which tracking algorithm to use. Possible
+        options are:
+            - SimpleSparseLAP
+            - SparseLAP
+            - Kalman
+            - AdvancedKalman
+            - NearestNeighbor
+            - Overlap
+    :param tracker_settings: Dictionary containing parameters for the specified
+        tracker. These should be written the same way they are in the TrackMate
+        GUI. See the source code for a full reference:
+            https://github.com/trackmate-sc/TrackMate/blob/master/src/main/java/fiji/plugin/trackmate/tracking/TrackerKeys.java.
+    :return: None, updates settings as a side-effect.
+    """
+    options = {
+        "SimpleSparseLAP": "fiji.plugin.trackmate.tracking.jaqaman.SimpleSparseLAPTrackerFactory",
+        "SparseLAP": "fiji.plugin.trackmate.tracking.jaqaman.SparseLAPTrackerFactory",
+        "Kalman": "fiji.plugin.trackmate.tracking.kalman.KalmanTrackerFactory",
+        "AdvancedKalman": "fiji.plugin.trackmate.tracking.kalman.AdvancedKalmanTrackerFactory",
+        "NearestNeighbor": "fiji.plugin.trackmate.tracking.kdtree.NearestNeighborTrackerFactory",
+        "Overlap": "fiji.plugin.trackmate.tracking.overlap.OverlapTrackerFactory",
+    }
+    try:
+        selected = options[tracker]
+    except KeyError as ex:
+        raise KeyError(f"tracker must be one of {','.join(options.keys())}") from ex
+
+    settings.trackerFactory = sj.jimport(selected)()
+    settings.trackerSettings = settings.trackerFactory.getDefaultSettings()
+    if tracker_settings is not None:
+        for k, v in tracker_settings.items():
+            # The automatic type conversion fails in some cases
+            if isinstance(v, dict):
+                hash_map = sj.jimport("java.util.HashMap")
+                val = hash_map(v)
+            elif isinstance(v, bool):
+                jbool = sj.jimport("java.lang.Boolean")
+                val = jbool(v)
+            elif isinstance(v, int):
+                jint = sj.jimport("java.lang.Integer")
+                val = jint(v)
+            else:
+                val = v
+            settings.trackerSettings[k] = val
 
 parser = argparse.ArgumentParser(
                     description='Tracks a given image'
@@ -21,10 +137,9 @@ config = json.loads(args.config)
 mask_dir = args.mask_dir
 tracker = config['algorithm']
 tracker_settings = config['settings']
-max_heap = requested_memory
 
 try:
-    setup_imagej(max_heap)
+    setup_imagej(requested_memory)
 
     imp = read_image_stack(mask_dir)
     settings = sj.jimport("fiji.plugin.trackmate.Settings")(imp)
