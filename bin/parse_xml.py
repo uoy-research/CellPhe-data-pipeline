1904d7ae85042d3fac29c5abb7db7110246d130d
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-11-19 12:40:26 +0000

    Working parse_xml

--- bin/parse_xml.py
+++ bin/parse_xml.py
@@ -8,6 +8,41 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import pandas as pd
 from roifile import ImagejRoi, roiwrite
+from skimage.draw import line
+
+def interpolate_between_points(coords: np.array) -> np.array:
+    """
+    Interpolates between the ROI coordinates to ensure there aren't any breaks
+    in the boundary.
+
+    All the downstream CellPhe analysis assumes that there aren't any gaps in
+    the ROIs. This function guarantees that.
+
+    :param coords: A 2D Numpy array of coordinate pairs in the form (x,y).
+    :return: An array in the same format as coords with either the same number
+        of coordinates, or more.
+    """
+    new_coords_raw = []
+    # Iterate through each consecutive coordinate pair interpolating a
+    # line between them
+    # NB: assumes that ROIs are stored in order, which they should be from
+    # TrackMate
+    for i in range(1, coords.shape[0]):
+        # These are in format (y, x) (i.e. row/column)
+        new_coords_raw.append(line(coords[i - 1, 1], coords[i - 1, 0], coords[i, 1], coords[i, 0]))
+    # Ensure first and last point are connected
+    new_coords_raw.append(line(coords[-1, 1], coords[-1, 0], coords[0, 1], coords[0, 0]))
+    # Convert back to a single 2D numpy array in (y,x) format
+    new_coords = np.concatenate([np.asarray(x).T for x in new_coords_raw])
+    # Remove duplicate coordinates
+    # Have to do a 2-step process as np.unique() sorts by default and cannot
+    # be told not to
+    _, inds = np.unique(new_coords, axis=0, return_index=True)
+    new_coords = new_coords[np.sort(inds)]
+
+    # Convert back to (x,y)
+    new_coords = np.flip(new_coords, axis=1)
+    return new_coords
 
 def save_rois(rois: list[dict], filename: str = "rois.zip"):
     """
